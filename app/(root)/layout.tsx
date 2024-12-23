f244696f7fe64c8418f76a23dcff23967b30fe14
Chao Chen <chaochen@live.com>
2024-12-23 13:55:41 -0500

    feat: desktop sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { redirect } from "next/navigation";
 
 import Header from "@/components/Header";
 import MobileNavigation from "@/components/MobileNavigation";
 import Sidebar from "@/components/Sidebar";
+import { getCurrentUser } from "@/lib/actions/user.actions";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return redirect("/sign-in");
+
   return (
     <main className="flex h-screen">
-      <Sidebar />
+      <Sidebar {...currentUser} />
       <section className="flex h-full flex-1 flex-col">
         <MobileNavigation /> <Header />
         <div className="main-content">{children}</div>
