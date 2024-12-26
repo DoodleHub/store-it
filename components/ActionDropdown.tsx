3290b793cb9ad3c678e3d0ad2e4140d1b8a66f20
Chao Chen <chaochen@live.com>
2024-12-26 14:58:20 -0500

    feat: delete files

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -27,7 +27,11 @@ import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { FileDetails, ShareInput } from "./ActionsModalContent";
 
-import { renameFile, updateFileUsers } from "@/lib/actions/file.actions";
+import {
+  deleteFile,
+  renameFile,
+  updateFileUsers,
+} from "@/lib/actions/file.actions";
 import { constructDownloadUrl } from "@/lib/utils";
 import { actionsDropdownItems } from "@/constants";
 
@@ -62,7 +66,8 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
       rename: () =>
         renameFile({ fileId: file.$id, name, extension: file.extension, path }),
       share: () => updateFileUsers({ fileId: file.$id, emails, path }),
-      delete: () => console.log("delete"),
+      delete: () =>
+        deleteFile({ fileId: file.$id, path, bucketFileId: file.bucketFileId }),
     };
 
     success = await actions[action.value as keyof typeof actions]();
@@ -109,6 +114,12 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
               onInputChange={setEmails}
               onRemove={handleRemoveUser}
             />
+          )}
+          {value === "delete" && (
+            <p className="delete-confirmation">
+              Are you sure you want to delete{" "}
+              <span className="delete-file-name">{file.name}</span>
+            </p>
           )}
         </DialogHeader>
         {["rename", "delete", "share"].includes(value) && (
