478ad883e3b6aa2450802614a8b757d66714bc39
Chao Chen <chaochen@live.com>
2024-12-26 15:33:11 -0500

    feat: search and sort query params

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -3,16 +3,20 @@ import { Models } from "node-appwrite";
 import Card from "@/components/Card";
 import Sort from "@/components/Sort";
 
-import { getFiles } from "@/lib/actions/file.actions";
+import { FileType, getFiles } from "@/lib/actions/file.actions";
+import { getFileTypesParams } from "@/lib/utils";
 
 interface SearchParamProps {
   params?: Promise<{ type: string }>;
   searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 
-const Page = async ({ params }: SearchParamProps) => {
+const Page = async ({ searchParams, params }: SearchParamProps) => {
   const type = (await params)?.type || "";
-  const files = await getFiles();
+  const searchText = ((await searchParams)?.query as string) || "";
+  const sort = ((await searchParams)?.sort as string) || "";
+  const types = getFileTypesParams(type) as FileType[];
+  const files = await getFiles({ types, searchText, sort });
 
   return (
     <div className="page-container">
